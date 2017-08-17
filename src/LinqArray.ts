ed89d6d3aa18715e4666d3805dd20a75ac1a2b89
wzy <twoeo@qq.com>
2017-08-17 20:17:52 +0800

    init

---
+++ src/LinqArray.ts
@@ -0,0 +1,151 @@
+///<reference path="LinqArray.d.ts"/>
+
+Array.prototype.remove = function (filter: (v: any, i: any, arr: any) => boolean | any): any[] {
+    if (typeof (filter) == 'function') {
+        for (let i = this.length - 1; i > -1; --i) {
+            filter(this[i], i, this) && this.splice(i, 1);
+        }
+    }
+    else {
+        for (let i = this.length - 1; i > -1; --i) {
+            this[i] === filter && this.splice(i, 1);
+        }
+    }
+
+    return this;
+}
+
+Array.prototype.removeOne = function (filter: (v: any, i: any, arr: any) => boolean | any): any[] {
+    if (typeof (filter) == 'function') {
+        for (let i = 0; i < this.length; ++i) {
+            if (filter(this[i], i, this)) {
+                this.splice(i, 1);
+                return this;
+            }
+        }
+    }
+    else {
+        for (let i = 0; i < this.length; ++i) {
+            if (this[i] === filter) {
+                this.splice(i, 1);
+                return this;
+            }
+        }
+    }
+
+    return this;
+}
+
+Array.prototype.first = function () {
+    return this.length ? this[0] : null;
+}
+
+Array.prototype.last = function () {
+    return this.length ? this[this.length - 1] : null;
+}
+
+Array.prototype.max = function <P>(mapper?: (v: any, i: any, arr: any) => P): P | null {
+    if (!this.length) {
+        return null;
+    }
+
+    function _max(a: any, b: any) {
+        return a > b ? a : b;
+    }
+
+    if (typeof (mapper) == 'function') {
+        let max: P = mapper(this[0], 0, this);
+        for (let i = 1; i < this.length; ++i) {
+            let temp = mapper(this[i], i, this);
+            max = temp > max ? temp : max;
+        }
+        return max;
+    }
+    else {
+        return this.reduce((prev, cur) => _max(prev, cur));
+    }
+}
+
+Array.prototype.min = function <P>(mapper?: (v: any, i: any, arr: any) => P): P | null {
+    if (!this.length) {
+        return null;
+    }
+
+    function _min(a: any, b: any) {
+        return a < b ? a : b;
+    }
+
+    if (typeof (mapper) == 'function') {
+        let min: P = mapper(this[0], 0, this);
+        for (let i = 1; i < this.length; ++i) {
+            let temp = mapper(this[i], i, this);
+            min = temp < min ? temp : min;
+        }
+        return min;
+    }
+    else {
+        return this.reduce((prev, cur) => _min(prev, cur));
+    }
+}
+
+Array.prototype.distinct = function () {
+    return this.filter((v, i, arr) => arr.indexOf(v) === i);
+}
+
+Array.prototype.filterIndex = function (filter: any) {
+    let output: number[] = [];
+    for (let i = 0; i < this.length; ++i) {
+        if (filter(this[i], i, this)) {
+            output.push(i);
+        }
+    }
+    return output;
+}
+
+Array.prototype.orderBy = function (...mappers: any[]) {
+    return this.slice().sort((a, b) => {
+        for (let i = 0; i < mappers.length; ++i) {
+            let va = mappers[i](a);
+            let vb = mappers[i](b);
+            if (va > vb) {
+                return 1;
+            }
+            else if (va < vb) {
+                return -1;
+            }
+        }
+        return 0;
+    });
+}
+
+Array.prototype.orderByDesc = function (...mappers: any[]) {
+    return this.slice().sort((a, b) => {
+        for (let i = 0; i < mappers.length; ++i) {
+            let va = mappers[i](a);
+            let vb = mappers[i](b);
+            if (va > vb) {
+                return -1;
+            }
+            else if (va < vb) {
+                return 1;
+            }
+        }
+        return 0;
+    });
+}
+
+Array.prototype.binIndexOf = function (filter: any) {
+    let low = 0,
+        high = this.length - 1;
+    while (low <= high) {
+        let mid = Math.floor(((high + low) / 2));
+        if (filter === this[mid]) {
+            return mid;
+        } else if (filter > this[mid]) {
+            low = mid + 1;
+        } else if (filter < this[mid]) {
+            high = mid - 1;
+        }
+    }
+    return -1;
+}
---
+++ test/LinqArray.test.ts
@@ -0,0 +1,77 @@
+import '../src/LinqArray';
+const assert = require('assert');
+
+describe('LinqArray', function(){
+    it('distinct', function(){
+        assert.deepEqual([1,1,2,3,2,3,3,2,3,1,3,4,2,5,3,4,3,1,4,5,1,2,3,4,5,1,4,3,1,3,2,4,1,5,3].distinct(),[1,2,3,4,5])
+        assert.deepEqual([1,'2',true,null,undefined,{},[1,2,3],1,'2',true,null,undefined,{},[1,2,3]].distinct(),
+            [1,'2',true,null,undefined,{},[1,2,3],{},[1,2,3]]
+        )
+    })
+
+    it('max', function () {
+        assert.equal([4,9,5,1,3].max(), 9);
+        assert.equal([4,9,5,1,3].max(v=>v*2), 18);
+        assert.equal([999999,1,2,3,6].max((v,i)=>v*i), 24);
+        assert.equal([4,9,5,1,3].max((v,i,arr)=>arr[i]+1), 10);
+    })
+
+    it('min', function () {
+        assert.equal([4,9,5,1,3].min(), 1);
+        assert.equal([4,9,5,1,3].min(v=>v*2), 2);
+        assert.equal([999999,1,2,3,6].min((v,i)=>v*i), 0);
+        assert.equal([4,9,5,1,3].min((v,i,arr)=>arr[i]+1), 2);
+    })
+
+    it('filterIndex', function(){
+        assert.deepEqual([10,20,30,40,50].filterIndex(v=>v>30), [3,4])
+        assert.deepEqual([13,14,16,18,20].filterIndex((v,i,arr)=>(v+i*arr.length)%2==1), [0,1,3]);
+    })
+
+    it('orderBy', function(){
+        let a = [
+            {a:6, b:5},
+            {a:4, b:3},
+            {a:6, b:1},
+            {a:2, b:9},
+            {a:4, b:7}
+        ];
+        assert.deepEqual(a.orderBy(v=>v.a), [
+            {a:2, b:9},
+            {a:4, b:3},
+            {a:4, b:7},
+            {a:6, b:5},
+            {a:6, b:1}
+        ]);
+        assert.deepEqual(a.orderBy(v=>v.a, v=>v.b), [
+            {a:2, b:9},
+            {a:4, b:3},
+            {a:4, b:7},
+            {a:6, b:1},
+            {a:6, b:5}
+        ]);
+        assert.deepEqual(a.orderByDesc(v=>v.b), [
+            {a:2, b:9},
+            {a:4, b:7},
+            {a:6, b:5},
+            {a:4, b:3},
+            {a:6, b:1}
+        ]);
+        assert.deepEqual(a.orderByDesc(v=>v.a, v=>v.b), [
+            {a:6, b:5},
+            {a:6, b:1},
+            {a:4, b:7},
+            {a:4, b:3},
+            {a:2, b:9}
+        ]);
+    }),
+
+    it('binIndexOf', function(){
+        let arr = [1,1,20,20,20,21,30,30,40,50,50,50];
+        assert.equal(arr.binIndexOf(30), 6)
+        assert.equal(arr.binIndexOf(31), -1)
+        assert.equal(arr.binIndexOf(1), 0)
+        assert.equal(arr.binIndexOf(50), 10)
+        assert.equal(arr.binIndexOf(20), 2)
+    })
+});
